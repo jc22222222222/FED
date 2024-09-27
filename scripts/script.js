c914117faa6bb79fc1867198cc5117fd8e44f1c1
Jean-Carlos2000 <152287711+Jean-Carlos2000@users.noreply.github.com>
2024-09-27 10:13:40 +0200

    updatde

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,10 @@
-// JavaScript Document
-console.log("hi");
+let deButton = document.querySelector("nav button:nth-of-type(2)");
+let deNav = document.querySelector("nav");
+
+
+deButton.onclick = toggleMenu;
+
+function toggleMenu(){
+  deNav.classList.toggle("toonMenu");
+}
+
